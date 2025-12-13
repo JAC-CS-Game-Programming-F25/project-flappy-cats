c74ae0b7ad29a4560a65d98fd2527ff924250358
Antonina Kosyakova <tonya.kosyakova@gmail.com>
2025-12-13 14:10:26 -0500

    removed old mario code

--- src/enums/GameStateName.js
+++ src/enums/GameStateName.js
@@ -1,13 +1,10 @@
 const GameStateName = {
 	GameOver: 'game-over',
 	Play: 'play',
-	Victory: 'victory',
-	//TitleScreen: 'title-screen',
 	Restore: 'restore',
-    Title: 'title',
-    CatSelect: 'cat-select',
-    Play: 'play',
-    Pause: 'pause',
+	Title: 'title',
+	CatSelect: 'cat-select',
+	Pause: 'pause',
 };
 
 export default GameStateName;
